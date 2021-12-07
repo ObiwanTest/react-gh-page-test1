e12147036c52bd40463217b7de5292510fe19cdd
ObiwanTest <test.obiwan.test@gmail.com>
2021-12-06 23:16:42 -0500

    Adding react strictmode back in

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
+  <React.StrictMode>
     <Router basename="/react-gh-page-test1">
       <App />
-    </Router>,
+    </Router>
+  </React.StrictMode>,
   document.getElementById('root')
 );
